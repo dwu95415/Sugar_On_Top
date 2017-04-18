5f948177b6efbcd3e0b7deb47831460e6326ec3a
Miguel Del Rio <drmiguel@mit.edu>
2017-04-18 14:21:53 -0400

    adding all the popup corrections

--- popup.js
+++ popup.js
@@ -1,3 +1,43 @@
+totalCarbs = 0;
+icr = 0;
+bloodSugar = 0;
+var updateInsulin = function()
+{
+  if(icr != 0)
+  {
+    console.log('doin good');
+    var ratio = Math.round(totalCarbs/icr);
+    var extra = 0;
+    if(bloodSugar >= 150)
+      Math.round(extra = (bloodSugar-100)/50);
+    var out = ratio + extra;
+    $('#insulin').html('Inject '+ out +' units');
+  }
+  else{
+    console.log('noooo');
+    $('#insulin').html('Inject 0 units');
+  }
+}
+
+$(document).on('keyup', function(evt) {
+  var focusedElem = document.activeElement;
+  if($(focusedElem).hasClass('input-num'))
+  {
+    if(focusedElem.id == 'total-carbs')
+    {
+      totalCarbs = $(focusedElem).val();
+    } else if(focusedElem.id == 'icr'){
+      icr = $(focusedElem).val();
+    } else if(focusedElem.id == 'bloodSugar'){
+      bloodSugar = $(focusedElem).val();
+    }
+    console.log(totalCarbs);
+    console.log(icr);
+    console.log(bloodSugar);
+    updateInsulin();
+  }
+});
+
 $(document).on('click','#calculate',function(){
   console.log('click');
   $('.popup-background').animate({
