ebb1c9e2ad12156f34b985629f3a791b60d42fcd
Ziad Baaklini <ziadb@mit.edu>
2017-04-18 08:00:14 -0400

    Add files via upload

--- Blood Sugar Canvas/Scripts/historyCanvas2.js
+++ Blood Sugar Canvas/Scripts/historyCanvas2.js
@@ -1,46 +1,34 @@
 var historyGraph = function () {
     var ctx;
     var margin = { top: 40, left: 75, right: 0, bottom: 75 };
-    var height, width, yMax, xMax, data;
+    var chartHeight, chartWidth, yMax, xMax, data;
     var maxYValue = 500;
-    var ratio = 1;
+    var ratio = 0;
     var renderType = { lines: 'lines', points: 'points' };
 
     var create = function(canvasId, dataObj) {
         data = dataObj;
+        //getMaxY();
         var canvas = document.getElementById(canvasId);
-        height = $("#canvas").height();
-        width = $("#canvas").width();
-        xMax = width - (margin.left + margin.right);
-        yMax = height - (margin.top + margin.bottom);
-        //ratio = yMax / maxYValue;
-
+        chartHeight = canvas.getAttribute('height');
+        chartWidth = canvas.getAttribute('width');
+        xMax = chartWidth - (margin.left + margin.right);
+        yMax = chartHeight - (margin.top + margin.bottom);
+        ratio = yMax / maxYValue;
         ctx = canvas.getContext("2d");
         createGraph();
     };
 
-
-    var createGraph = function() {
+    var createGraph = function () {
         renderText();
         createLabels();
-    };
 
-    var traceFigure = function(canvasID, meal, dataObj){
-
-        var figureData = dataObj;
-        var dataPoints = meal.dataPoints;
         //render data based upon type of renderType(s) that client supplies
-        if (figureData.renderTypes == undefined || figureData.renderTypes == null) figureData.renderTypes = [renderType.lines];
-        for (var i = 0; i < figureData.renderTypes.length; i++) {
-            drawData(figureData.renderTypes[i],dataPoints,canvasID);
+        if (data.renderTypes == undefined || data.renderTypes == null) data.renderTypes = [renderType.lines];
+        for (var i = 0; i < data.renderTypes.length; i++) {
+            drawData(data.renderTypes[i]);
         }
     };
-
-    /*$(document).on('mousemove', '#canvas', function(evt){
-        var x = evt.pageX - $("canvas").offset().left;
-        var y = evt.pageY - $("#canvas").offset().top;
-
-    });*/
 
     /*var getMaxY = function () {
         for (var i = 0; i < data.dataPoints.length; i++) {
@@ -57,12 +45,12 @@ var historyGraph = function () {
 
         //Title
         var txtSize = ctx.measureText(data.title);
-        ctx.fillText(data.title, (width / 2), (margin.top / 2));
+        ctx.fillText(data.title, (chartWidth / 2), (margin.top / 2));
 
         //X-axis text
         txtSize = ctx.measureText(data.xLabel);
         ctx.fillText(data.xLabel, margin.left + (xMax / 2) - (txtSize.width / 2), yMax + (margin.bottom / 1.2));
-      
+
         //Y-axis text
         ctx.save();
         ctx.rotate(-Math.PI / 2);
@@ -71,44 +59,16 @@ var historyGraph = function () {
         ctx.restore();
     };
 
-    var getDates = function(){
-        var dates = [];
-        for (var i = 0; i < 15; i++){
-            dates[i] = getDate(i);
-        }
-
-        return dates;
-    };
-
-    var getDate = function(index){
-        var today = new Date();
-        today.setDate(today.getDate() - index);
-        var dd = today.getDate();
-        var mm = today.getMonth()+1; //January is 0!
-
-        if(dd<10) {
-            dd='0'+dd
-        } 
-
-        if(mm<10) {
-            mm='0'+mm
-        }    
-
-            today = mm+'/'+dd 
-        return today;
-    };
-
     var createLabels = function () {
         //Vertical guide lines
         var yInc = 50;
         var yPos = 0;
         var yLabelInc = 25;
-        var xInc = 50;
+        var xInc = 5;0
         var xPos = margin.left;
         ymax = 500;
-        dates = getDates();
-        for (var i = 0; i < 10; i++) {
-            yPos += (i == 0) ? 0 : yInc;
+        for (var i = 0; i < 15; i++) {
+            yPos += (i == 0) ? margin.top : yInc;
 
             //y axis labels
             ctx.font = (data.dataPointFont != null) ? data.dataPointFont : '10pt Calibri';
@@ -117,7 +77,7 @@ var historyGraph = function () {
             ctx.fillText(txt, margin.left - ((txtSize.width >= 14) ? txtSize.width : 10) - 7, yPos);
 
             //x axis labels
-            txt = dates[i];
+            txt = data.dataPoints[i].x;
             txtSize = ctx.measureText(txt);
             ctx.fillText(txt, xPos, ymax + (margin.bottom / 3));
             xPos += xInc;
@@ -127,31 +87,53 @@ var historyGraph = function () {
         drawLine(margin.left, margin.top, margin.left, yMax, 'black');
 
         //Horizontal Line
-        drawLine(margin.left, yMax + 15, xMax, yMax + 15, 'black');
+        drawLine(margin.left, yMax, xMax, yMax, 'black');
     };
 
-    var drawData = function(type, dataPoints, canvasID) {
+    /*var createLabels = function () {
+        //Vertical guide lines
+        var yInc = yMax / data.dataPoints.length;
+        var yPos = 0;
+        var yLabelInc = (maxYValue * ratio) / data.dataPoints.length;
+        var xInc = getXInc();
+        var xPos = margin.left;
+        for (var i = 0; i < data.dataPoints.length; i++) {
+            yPos += (i == 0) ? margin.top : yInc;
+
+            //y axis labels
+            ctx.font = (data.dataPointFont != null) ? data.dataPointFont : '10pt Calibri';
+            var txt = Math.round(maxYValue - ((i == 0) ? 0 : yPos / ratio));
+            var txtSize = ctx.measureText(txt);
+            ctx.fillText(txt, margin.left - ((txtSize.width >= 14) ? txtSize.width : 10) - 7, yPos + 4);
+
+            //x axis labels
+            txt = data.dataPoints[i].x;
+            txtSize = ctx.measureText(txt);
+            ctx.fillText(txt, xPos, yMax + (margin.bottom / 3));
+            xPos += xInc;
+        }
+
+        //Vertical line
+        drawLine(margin.left, margin.top, margin.left, yMax, 'black');
+
+        //Horizontal Line
+        drawLine(margin.left, yMax, xMax, yMax, 'black');
+    };*/
+
+    var drawData = function(type) {
         var xInc = getXInc();
         var prevX = 0, 
             prevY = 0;
 
-        for (var i = 0; i < 15; i++) {
-            var pt = dataPoints[i];
+        for (var i = 0; i < data.dataPoints.length; i++) {
+            var pt = data.dataPoints[i];
             var ptY = (maxYValue - pt.y) * ratio;
             if (ptY < margin.top) ptY = margin.top;
             var ptX = (i * xInc) + margin.left;
 
             if (i > 0 && type == renderType.lines) {
                 //Draw connecting lines
-                var color;
-                if (canvasID == "breakfast")
-                    color = "red";
-                if (canvasID == "lunch")
-                    color = "blue";
-                if (canvasID == "dinner")
-                    color = "green";
-
-                drawLine(ptX, ptY, prevX, prevY, color, 2);
+                drawLine(ptX, ptY, prevX, prevY, 'black', 2);
             }
 
             if (type == renderType.points) {
@@ -160,7 +142,7 @@ var historyGraph = function () {
                 ctx.beginPath();
                 ctx.fillStyle = radgrad;
                 //Render circle
-                ctx.arc(ptX, ptY, 4, 0, 2 * Math.PI, false)
+                ctx.arc(ptX, ptY, 8, 0, 2 * Math.PI, false)
                 ctx.fill();
                 ctx.lineWidth = 1;
                 ctx.strokeStyle = '#000';
@@ -174,7 +156,7 @@ var historyGraph = function () {
     };
 
     var getXInc = function() {
-        return Math.round(xMax / 15) - 1;
+        return Math.round(xMax / data.dataPoints.length) - 1;
     };
 
     var drawLine = function(startX, startY, endX, endY, strokeStyle, lineWidth) {
@@ -189,9 +171,9 @@ var historyGraph = function () {
 
     return {
         renderType: renderType,
-        create: create,
-        traceFigure: traceFigure
+        create: create
     };
 
     
 } ();
+s
