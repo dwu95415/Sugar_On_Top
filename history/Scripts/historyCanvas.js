a5a915c800306367112173d6e2eac48f2df31632
Miguel Del Rio <drmiguel@mit.edu>
2017-04-27 21:28:26 -0400

    some small changes

--- history/Scripts/historyCanvas.js
+++ history/Scripts/historyCanvas.js
@@ -200,7 +200,7 @@
 
 $(function () {
     var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"],
     datasets: [
         {
             label: "Breakfast",
