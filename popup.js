e69e674538cfade305e39c43cb3c2c6931684f9b
Ziad Baaklini <ziadb@mit.edu>
2017-05-08 17:39:28 -0400

    Relevant changes

--- popup.js
+++ popup.js
@@ -1,6 +1,10 @@
 totalCarbs = 0;
 icr = 0;
 bloodSugar = 0;
+var out = 0;
+
+var day = 0;
+
 var updateInsulin = function()
 {
   if(icr != 0 && bloodSugar != 0)
@@ -19,7 +23,7 @@ var updateInsulin = function()
     var extra = 0;
     if(bloodSugar >= 150)
       extra = Math.floor((bloodSugar-100)/50);
-    var out = ratio + extra;
+    out = ratio + extra;
     $('#insulin').html('Inject '+ out +' units');
   }
   else{
@@ -160,9 +164,54 @@ $(document).on('click','#save-to-history',function(){
   }, 250,function(){
     remove();
     $('.popup').css('z-index',-1);
+
+
   });
 
+
+  updateData(out);
+
 });
+
+var updateData = function(out){
+
+  var breakfast_data = JSON.parse(localStorage.getItem('breakfast'));
+  var lunch_data = JSON.parse(localStorage.getItem('lunch'));
+  var dinner_data = JSON.parse(localStorage.getItem('dinner'));
+
+  var data;
+  var date = new Date();
+  var hour = date.getHours();
+  if (hour < 11 && hour > 3){
+      data = breakfast_data;
+  }
+  else if (hour > 11 && hour < 5){
+    data = lunch_data;
+  }
+  else
+    data = dinner_data
+  
+  if (date.length == 7 && date.getDay() != JSON.parse(localStorage.getItem('day'))){
+    popData(dinner_data);
+    popData(breakfast_data);
+    popData(lunch_data);
+    localStorage.setItem('day',JSON.stringify(date.getDay()));
+  }
+
+
+  data.push([data.length + 1,out]);
+
+  localStorage.setItem('breakfast', JSON.stringify(breakfast_data));
+  localStorage.setItem('lunch', JSON.stringify(lunch_data));
+  localStorage.setItem('dinner', JSON.stringify(dinner_data));
+}
+
+var popData = function(data){
+  for (var i = 0; i < data.length; i++){
+    data[i][0] = data[i][0] - 1
+  }
+  data.shift();
+}
 
 var remove = function(){
   $('.popup-background').addClass('invisible');
